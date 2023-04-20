b7d82fe68fd274a9119b3771e5515486722b924c
Krish4856 <98867214+Krish4856@users.noreply.github.com>
2023-04-20 11:52:33 +0530

    Implemented Swagger Documentation

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+// Swagger
+const swaggerUI = require('swagger-ui-express')
+const yaml = require('yamljs')
+const swaggerDocument = yaml.load('./swagger.yaml')
+
 const express = require('express');
 const app = express();
 
@@ -35,8 +40,10 @@ app.use(xss())
 
 // routes
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('<h1>Welcome to Jobs API. Head over to the <a href="/api-docs">API Documentation (Swagger)</a></h1>')
 })
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs',authMiddleware, jobRouter)
 
